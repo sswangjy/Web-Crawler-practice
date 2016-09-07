81c044a2418357182d820df6ef66dded2963f5dd
sswangjy <wangjy_9237@126.com>
2016-09-07 10:02:27 +0800

    Update listed company.R

--- listed company.R
+++ listed company.R
@@ -3,93 +3,93 @@ library(dplyr)
 library(rjson)
 library(XML)
 
-#############################×¥È¡¹ÉÆ±´úÂë#############################
+#############################æŠ“å–è‚¡ç¥¨ä»£ç #############################
 URL <- "http://quote.eastmoney.com/stocklist.html"
 htmldetail<-getURL(URL,.encoding = "gb2312")
 name <- strsplit(htmldetail,"\r\n")[[1]]
-#ÌáÈ¡Ò³ÃæÖĞ°üº¬¹ÉÆ±´úÂëµÄĞÅÏ¢
+#æå–é¡µé¢ä¸­åŒ…å«è‚¡ç¥¨ä»£ç çš„ä¿¡æ¯
 name <- name[grep("<li><a target=\\\"_blank\\\" href=\\\"http://quote.eastmoney.com/",name)]
-#ÌáÈ¡¹ÉÆ±´úÂëĞÅÏ¢
+#æå–è‚¡ç¥¨ä»£ç ä¿¡æ¯
 n <- length(name)
 total <- vector(mode = "character",length = n)
 tradadd <- vector(mode = "character",length = n)
 stkdata=data.frame(matrix(NA,n,6))
-colnames(stkdata)=c('¹ÉÆ±','¹ÉÆ±´úÂë','½»Ò×Ëù±êÊ¶','×¢²áµØÖ·','¾­¶È','Î¬¶È')
+colnames(stkdata)=c('è‚¡ç¥¨','è‚¡ç¥¨ä»£ç ','äº¤æ˜“æ‰€æ ‡è¯†','æ³¨å†Œåœ°å€','ç»åº¦','ç»´åº¦')
 for(i in 1:n) {
   n_str <- regexpr("\\\">",name[i])
   n_end <- regexpr("</a>",name[i])
   total[i] <- substr(name[i],n_str[1]+2,n_end[1]-1)
-  stkdata$½»Ò×Ëù±êÊ¶ <- substr(name[i],69,70)
+  stkdata$äº¤æ˜“æ‰€æ ‡è¯† <- substr(name[i],69,70)
 }
-stkdata$¹ÉÆ± <- substr(total,1,nchar(total)-8)
-stkdata$¹ÉÆ±´úÂë <- substr(total,nchar(total)-6,nchar(total)-1)
+stkdata$è‚¡ç¥¨ <- substr(total,1,nchar(total)-8)
+stkdata$è‚¡ç¥¨ä»£ç  <- substr(total,nchar(total)-6,nchar(total)-1)
 write.csv(stkdata,file="baidu_geocoding.csv")
 rm(total,n_str,n_end,tradadd)
-##############################×¥È¡µØÖ·
+##############################æŠ“å–åœ°å€
 
-URL <- paste0("http://f10.eastmoney.com/f10_v2/CompanySurvey.aspx?code=",stkdata$½»Ò×Ëù±êÊ¶,stkdata$¹ÉÆ±´úÂë,"#jbzl-0")
-for (i in 3789:length(URL) ){
+URL <- paste0("http://f10.eastmoney.com/f10_v2/CompanySurvey.aspx?code=",stkdata$äº¤æ˜“æ‰€æ ‡è¯†,stkdata$è‚¡ç¥¨ä»£ç ,"#jbzl-0")
+for (i in 1:length(URL) ){
   #Sys.sleep(runif(1,1,2))
   doc <- htmlParse(URL[i],encoding="UTF-8")
   rootNode <- xmlRoot(doc)
   tables <- readHTMLTable(doc,header=F,which=1)
   tables_df <- tbl_df(tables)
-  stkdata$×¢²áµØÖ·[i] <- as.character(filter(tables_df,V3=="×¢²áµØÖ·")$V4)
+  stkdata$æ³¨å†Œåœ°å€[i] <- as.character(filter(tables_df,V3=="æ³¨å†Œåœ°å€")$V4)
   cat(i,"\n")
 }
-stkdata <- cbind(paste0(stkdata$½»Ò×Ëù±êÊ¶,stkdata$¹ÉÆ±´úÂë),stkdata)
-colnames(stkdata)=c('unicode','¹ÉÆ±','¹ÉÆ±´úÂë','½»Ò×Ëù±êÊ¶','×¢²áµØÖ·','¾­¶È','Î¬¶È')
+stkdata <- cbind(paste0(stkdata$äº¤æ˜“æ‰€æ ‡è¯†,stkdata$è‚¡ç¥¨ä»£ç ),stkdata)
+colnames(stkdata)=c('unicode','è‚¡ç¥¨','è‚¡ç¥¨ä»£ç ','äº¤æ˜“æ‰€æ ‡è¯†','æ³¨å†Œåœ°å€','ç»åº¦','ç»´åº¦')
 
 stkdata_df <- tbl_df(stkdata)
-a<-filter(stkdata_df,substr(stkdata_df$¹ÉÆ±´úÂë,1,3)=="600")
-b<-filter(stkdata_df,substr(stkdata_df$¹ÉÆ±´úÂë,1,3)=="601")
-c<-filter(stkdata_df,substr(stkdata_df$¹ÉÆ±´úÂë,1,3)=="000")
-d<-filter(stkdata_df,substr(stkdata_df$¹ÉÆ±´úÂë,1,3)=="603")
-e<-filter(stkdata_df,substr(stkdata_df$¹ÉÆ±´úÂë,1,3)=="002")
-f<-filter(stkdata_df,substr(stkdata_df$¹ÉÆ±´úÂë,1,3)=="300")
+a<-filter(stkdata_df,substr(stkdata_df$è‚¡ç¥¨ä»£ç ,1,3)=="600")
+b<-filter(stkdata_df,substr(stkdata_df$è‚¡ç¥¨ä»£ç ,1,3)=="601")
+c<-filter(stkdata_df,substr(stkdata_df$è‚¡ç¥¨ä»£ç ,1,3)=="000")
+d<-filter(stkdata_df,substr(stkdata_df$è‚¡ç¥¨ä»£ç ,1,3)=="603")
+e<-filter(stkdata_df,substr(stkdata_df$è‚¡ç¥¨ä»£ç ,1,3)=="002")
+f<-filter(stkdata_df,substr(stkdata_df$è‚¡ç¥¨ä»£ç ,1,3)=="300")
 stkdata_a <- rbind(a,b,c,d,e,f)
 rm(a,b,c,d,e,f,)
 
 write.csv(stkdata,file="f:/stkdataall.csv")
 write.csv(stkdata_a,file="f:/stkdata_a.csv")
 
-#############################×¥È¡¾­Î³¶È
+#############################æŠ“å–ç»çº¬åº¦
 stkdata <- read.csv("...\\test.csv")
 AK <- "Your baidu AK"
 add <- stkdata$RegAddr
 n<-length(add)
 
-#ÁĞ±íÑ­»·-begin#
+#åˆ—è¡¨å¾ªç¯-begin#
 
 for (i in 1:length(add)) {
-  #½¨Á¢µØÖ·×ª»»ÍøÖ·
+  #å»ºç«‹åœ°å€è½¬æ¢ç½‘å€
   url <- paste("http://api.map.baidu.com/geocoder/v2/?ak=",AK,"&output=json&address=",add[i], sep = "")
   url_string <- URLencode(url)
   
-  # ²¶»ñÁ¬½Ó¶ÔÏó
+  # æ•è·è¿æ¥å¯¹è±¡
   connect <- url(url_string)
   
-  # ´¦Àíjson¶ÔÏó
+  # å¤„ç†jsonå¯¹è±¡
   temp_geo <- fromJSON(paste(readLines(connect,warn = F), collapse = ""))
   if(is.null(temp_geo$result$location$lat)){next}
   stkdata$lat[i]<-temp_geo$result$location$lat
   stkdata$lng[i]<-temp_geo$result$location$lng
   cat(i,"\n")
 }
-#ÁĞ±íÑ­»·-end#
+#åˆ—è¡¨å¾ªç¯-end#
 stkdata_a<-read.csv(file="baidu_geocoding3.csv")
-#####×Ô¶¯ÔËĞĞ-end#####
+#####è‡ªåŠ¨è¿è¡Œ-end#####
 
-#²é¿´Êı¾İ
+#æŸ¥çœ‹æ•°æ®
 e <- which(is.na(stkdata$lat))
 for (i in e){
   url <- paste("http://api.map.baidu.com/geocoder/v2/?ak=",AK,"&output=json&address=",add[i], sep = "")
   url_string <- URLencode(url)
   
-  # ²¶»ñÁ¬½Ó¶ÔÏó
+  # æ•è·è¿æ¥å¯¹è±¡
   connect <- url(url_string)
   
-  # ´¦Àíjson¶ÔÏó
+  # å¤„ç†jsonå¯¹è±¡
   temp_geo <- fromJSON(paste(readLines(connect,warn = F), collapse = ""))
   if(is.null(temp_geo$result$location$lat)){next}
   stkdata$lat[i]<-temp_geo$result$location$lat
@@ -99,7 +99,7 @@ for (i in e){
 
 
 
-#µ¼³öÊı¾İ
+#å¯¼å‡ºæ•°æ®
 write.csv(stkdata,file="...\\test_return.csv")
 
 
